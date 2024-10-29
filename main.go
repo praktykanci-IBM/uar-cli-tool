a89b13a608122448ce1ceb633842772f69447297
wiktorsiwecki <wiktorsiwecki123@gmail.com>
2024-10-29 11:20:00 +0100

    added alias for help flag and made the help information generate itself based on existing flags

--- main.go
+++ main.go
@@ -7,11 +7,17 @@ import (
 
 
 func main() {
-	var helpFlag = flag.Bool("help",false,"display application's flags")
+	var helpFlag = flag.Bool("help",false,"display application flags") //help flag
+
+	flag.BoolVar(helpFlag,"h",false,"alias for help flag") //alias h for help flag
+
 	flag.Parse()
 
-	if(*helpFlag){
-		fmt.Printf("Commands: \n\thelp: \tdisplay application's flags")
-		return 
+	if *helpFlag {
+		fmt.Println("Commands:")
+		flag.VisitAll(func(f *flag.Flag) {
+			fmt.Printf("\t%s\t %s\n", f.Name, f.Usage)
+		})
+		return
 	}
 }
