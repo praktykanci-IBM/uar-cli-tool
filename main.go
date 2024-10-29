a0ce1bec81a4dbc17256c93d304573995445b6e0
wiktorsiwecki <wiktorsiwecki123@gmail.com>
2024-10-29 11:08:25 +0100

    added help flag

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+)
 
+
+func main() {
+	var helpFlag = flag.Bool("help",false,"display application's flags")
+	flag.Parse()
+
+	if(*helpFlag){
+		fmt.Printf("Commands: \n\thelp: \tdisplay application's flags")
+		return 
+	}
 }
