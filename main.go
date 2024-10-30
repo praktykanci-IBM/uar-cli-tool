8c39dfce5eb5d0ea8b4451c7a7974a988ce93100
mikey <michal_maslanka@onet.pl>
2024-10-30 11:33:21 +0100

    added handling of .env file

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"praktykanci/uar/cmd"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+	
 	cmd.Execute()
 }
