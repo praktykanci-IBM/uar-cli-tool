8c2c8d009ece1b82825fdb1bf4b63c6a3e871488
wiktorsiwecki <wiktorsiwecki123@gmail.com>
2024-10-30 13:02:11 +0100

    made the request command update the file

--- cmd/request.go
+++ cmd/request.go
@@ -1,12 +1,29 @@
 package cmd
 
 import (
+	"bytes"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/spf13/cobra"
 )
+
+type Request struct {
+	Name          string `json:"name"`
+	When          int64  `json:"when"`
+	Justification string `json:"justification"`
+	Repo          string `json:"repo"`
+	ID			  string `json:"id"`
+}
+type Requests struct {
+	Requests []Request `json:"requests"`
+}
+
 
 var requestCmd = &cobra.Command{
     Use:     "request",
@@ -59,10 +76,90 @@ var requestCmd = &cobra.Command{
 			if message, ok := result["message"]; ok && message == "Not Found" {
 				fmt.Println("Repo does not exist.")
 			} else {
-				//user and repo exist
-				// fmt.Printf("Input data is %s %s %s.", args[0], args[1], args[2])
-				// url := "https://api.github.com/repos/praktykanci-IBM/user-access-records/contents/awaiting.json"
+				url := "https://api.github.com/repos/praktykanci-IBM/user-access-records/contents/requests.json"
+				githubToken := os.Getenv("GITHUB_PAT")
 
+				resp, err := http.Get(url)
+				if err != nil {
+					fmt.Println("Error:", err)
+					return
+				}
+				defer resp.Body.Close()
+
+				var result map[string]interface{}
+				if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+					fmt.Println("Error decoding JSON:", err)
+					return
+				}
+
+
+				if encodedContent, ok := result["content"].(string); ok {
+					decodedContent, err := base64.StdEncoding.DecodeString(encodedContent)
+					if err != nil {
+						fmt.Println("Error decoding Base64 content:", err)
+						return
+					}
+					sha:=result["sha"].(string)
+					var data Requests
+					errr:=json.Unmarshal(decodedContent, &data)
+					if errr!=nil{
+						fmt.Println("Error", errr)
+						return
+					}
+					id := uuid.New().String()
+					newRequest := Request{
+						Name:          username,
+						When:          time.Now().Unix(),
+						Justification: args[2],
+						Repo:          repo,
+						ID:            id,
+					}
+					
+					data.Requests = append(data.Requests, newRequest)
+					
+					jsonData, err := json.Marshal(data)
+					if err!=nil{
+						fmt.Println("Error: ",err)
+						return
+					}	
+					encodedData := base64.StdEncoding.EncodeToString(jsonData)
+
+
+					body:= map[string]string{
+						"message": "Add new request",
+						"content": encodedData,
+						"sha": sha,
+					}
+
+					bodyData, _ := json.Marshal(body)
+
+					req,err := http.NewRequest("PUT",url,bytes.NewBuffer(bodyData))
+					if(err!=nil){
+						fmt.Println("Error: ",err)
+						return
+					}
+					req.Header.Set("Authorization","Bearer "+githubToken)
+					req.Header.Set("Content-Type","application/json")
+
+					client := &http.Client{}
+
+					resp, err := client.Do(req)
+					if err != nil {
+						fmt.Println("Error making PUT request:", err)
+						return
+					}
+					defer resp.Body.Close()
+
+					if resp.StatusCode == http.StatusOK || resp.StatusCode == http.StatusCreated {
+						fmt.Println("Request added successfully.")
+						fmt.Println("ID of your request:", newRequest.ID)
+					} else {
+						fmt.Printf("Failed to update file. Status: %s\n", resp.Status)
+					}
+
+				} else {
+					fmt.Println("Content field not found or is not a string.")
+				}
 				
 			}
 		}
