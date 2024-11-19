4b86e71ac92180a33b0f25762f62a45acc96dc90
wiktorsiwecki <wiktorsiwecki123@gmail.com>
2024-11-19 13:03:36 +0100

    you can now validate using PR's

--- cmd/cbn/extract.go
+++ cmd/cbn/extract.go
@@ -117,7 +117,7 @@ var extractCmd = &cobra.Command{
 			os.Exit(1)
 		}
 
-		_, _, err = githubClient.Repositories.UpdateFile(context.Background(), configData.ORG_NAME, configData.DB_NAME, fmt.Sprintf("CBN/%s", *cbnOriginalFile.Name), &github.RepositoryContentFileOptions{
+		a, _, err := githubClient.Repositories.UpdateFile(context.Background(), configData.ORG_NAME, configData.DB_NAME, fmt.Sprintf("CBN/%s", *cbnOriginalFile.Name), &github.RepositoryContentFileOptions{
 			Message: github.String(fmt.Sprintf("Extract data for the CBN %s", cbnID)),
 			Content: resCbnMarshaled,
 			SHA:     cbnOriginalFile.SHA,
@@ -125,6 +125,125 @@ var extractCmd = &cobra.Command{
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 			os.Exit(1)
+		}
+
+		sha := a.Content.SHA
+
+		for _, user := range usersWithAccess {
+
+			requestFile, _, _, err := githubClient.Repositories.GetContents(context.Background(), configData.ORG_NAME, configData.DB_NAME, fmt.Sprintf("user-access-records/%s/%s/%s", strings.Split(cbnContent.Repo, "/")[0], strings.Split(cbnContent.Repo, "/")[1], *user.Name), nil)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error: %v\n", err) // no such request
+				os.Exit(1)
+			}
+			requestFileMarshaled, err := requestFile.GetContent()
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+				os.Exit(1)
+			}
+
+			var requestFileContent types.RequestDataCompleted
+			err = yaml.Unmarshal([]byte(requestFileMarshaled), &requestFileContent)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+				os.Exit(1)
+			}
+			var prCbnContent types.CbnData
+			tmp, err := yaml.Marshal(cbnContent)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+				os.Exit(1)
+			}
+			err = yaml.Unmarshal(tmp, &prCbnContent)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+				os.Exit(1)
+			}
+			for i, userInCbn := range prCbnContent.Users {
+				// fmt.Println(*user.Name, userInCbn.Name)
+				if *user.Name == fmt.Sprintf("%s.yaml", userInCbn.Name) {
+					if cbnContent.Type == "positive" {
+						prCbnContent.Users[i].State = types.Aproved
+					} else {
+						prCbnContent.Users[i].State = types.Rejected
+					}
+					fmt.Println("zmiana")
+					fmt.Println(prCbnContent.Users)
+					fmt.Println(cbnContent.Users)
+					// currentTime := time.Now()
+					// formattedTime := currentTime.Format("02.01.2006, 15:04 MST")
+
+					// validatedBy, _, err := githubClient.Users.Get(context.Background(), "")
+					// if err != nil {
+					// 	fmt.Println("Error:", err)
+					// 	return
+					// }
+
+					// cbnContent.Users[i].ValidatedBy = *validatedBy.Login
+					// cbnContent.Users[i].ValidatedOn = formattedTime
+
+					break
+				}
+			}
+
+			branchName := fmt.Sprintf("CBN/%s/%s", cbnContent.Repo, *user.Name)
+
+			_, _, err = githubClient.Repositories.GetBranch(context.Background(), configData.ORG_NAME, configData.DB_NAME, branchName, 0)
+			if err != nil {
+				baseRef, _, err := githubClient.Git.GetRef(context.Background(), configData.ORG_NAME, configData.DB_NAME, "refs/heads/main")
+				if err != nil {
+					fmt.Println("Error fetching base reference:", err)
+					return
+				}
+
+				newRef := &github.Reference{
+					Ref:    github.String("refs/heads/" + branchName),
+					Object: &github.GitObject{SHA: baseRef.Object.SHA},
+				}
+				_, _, err = githubClient.Git.CreateRef(context.Background(), configData.ORG_NAME, configData.DB_NAME, newRef)
+				if err != nil {
+					fmt.Println("Error creating branch:", err)
+					return
+				}
+				fmt.Println("Branch created successfully!")
+
+				fmt.Println(prCbnContent.Users)
+				fmt.Println(cbnContent.Users)
+			} else {
+				fmt.Println("This request already exists!")
+				return
+			}
+
+			prCbnMarshaled, err := yaml.Marshal(prCbnContent)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+				os.Exit(1)
+			}
+
+			_, _, err = githubClient.Repositories.UpdateFile(context.Background(), configData.ORG_NAME, configData.DB_NAME, fmt.Sprintf("CBN/%s", *cbnOriginalFile.Name), &github.RepositoryContentFileOptions{
+				Message: github.String(fmt.Sprintf("Extract data for the CBN %s", cbnID)),
+				Content: prCbnMarshaled,
+				SHA:     sha,
+				Branch:  github.String(branchName),
+			})
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+				os.Exit(1)
+			}
+
+			newPR := &github.NewPullRequest{
+				Title: github.String(fmt.Sprintf("Validate CBN - %s", cbnID)),
+				Head:  github.String(branchName),
+				Base:  github.String("main"),
+				Body:  github.String("Validate user"),
+			}
+
+			_, _, err = githubClient.PullRequests.Create(context.Background(), configData.ORG_NAME, configData.DB_NAME, newPR)
+			if err != nil {
+				fmt.Println("Error creating pull request:", err)
+				return
+			}
+
 		}
 
 		fmt.Printf("Data extracted for CBN with ID: %s\n", cbnID)
